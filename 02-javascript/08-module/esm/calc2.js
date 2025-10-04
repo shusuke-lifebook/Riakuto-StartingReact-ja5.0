b932e50fb9e2edcc1d66a848232597abb48b562b
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-10-04 20:23:41 +0900

    第二章の学習完了

---
+++ 02-javascript/08-module/esm/calc2.js
@@ -0,0 +1,5 @@
+import { add, multiply, TWO, TEN, Geman } from "./modules/index.js";
+
+console.log(add(5, TWO));
+console.log(multiply(4, TEN));
+console.log(Geman.FUNF, Geman.DREI);
---
+++ 02-javascript/08-module/esm/modules/index.js
@@ -0,0 +1,3 @@
+export * from "./constants.js";
+export { plus as add, default as multiply } from "./math.js";
+export * as Geman from "./constants2.js";
