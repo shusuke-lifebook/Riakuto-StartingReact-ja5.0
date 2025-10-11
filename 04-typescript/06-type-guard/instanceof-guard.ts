5a84efc09fed602747e1c828bc70af2de9ec0c45
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-10-11 16:16:00 +0900

    06-type-guardの学習完了

---
+++ 04-typescript/06-type-guard/instanceof-guard.ts
@@ -0,0 +1,29 @@
+class Base {
+  common = "common";
+}
+
+class Foo extends Base {
+  foo() {
+    console.log("foo");
+  }
+}
+
+class Bar extends Base {
+  bar() {
+    console.log("bar");
+  }
+}
+
+const doDrive = (arg: Foo | Bar) => {
+  if (arg instanceof Foo) {
+    arg.foo();
+    // arg.bar();
+  } else {
+    arg.bar();
+    // arg.foo();
+  }
+  console.log(arg.common);
+};
+
+doDrive(new Foo());
+doDrive(new Bar());
