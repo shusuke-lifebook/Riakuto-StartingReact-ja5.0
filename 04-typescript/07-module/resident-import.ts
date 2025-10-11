24676ff6aba8e70147a254beafe32adc0f2b7adc
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-10-11 16:46:38 +0900

    07-moduleの学習完了

---
+++ 04-typescript/07-module/resident-import.ts
@@ -0,0 +1,10 @@
+import { Resident } from "./resident-export";
+
+// const resident = new Resident(); // エラー
+const patty: Resident = {
+  name: "Patty Rabbit",
+  age: 8,
+  species: "rabbit",
+};
+
+console.log(patty);
