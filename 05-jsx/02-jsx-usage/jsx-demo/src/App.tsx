5887e33b3e828359e22801c8b5478b54b8bd07fe
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-10-11 18:56:03 +0900

    jsxの学習開始

---
+++ 05-jsx/02-jsx-usage/jsx-demo/src/App.tsx
@@ -0,0 +1,23 @@
+import Greet from "./components/Greet";
+import Summary from "./components/Summary";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Greet name="Patty" times={4} />
+      <Summary title="Maple Town" folded>
+        <p>
+          Patty Hope-rabbit, along with her family, arrives in Maple Town, a
+          smalltown inhabited by friendly animals.
+        </p>
+        <p>
+          Soon, the Rabbit Family settles in Maple Town as mail carriers and the
+          bitter, yet sweet friendship of Patty and Bobby begins to blossom.
+        </p>
+      </Summary>
+    </>
+  );
+}
+
+export default App;
---
+++ 05-jsx/02-jsx-usage/jsx-demo/src/components/Summary.tsx
@@ -0,0 +1,21 @@
+import type React from "react";
+import "./Summary.css";
+
+interface Props {
+  title: string;
+  folded?: boolean;
+  children: React.ReactNode;
+}
+
+function Summary({ title, folded = false, children }: Props) {
+  console.log(children);
+
+  return (
+    <details className="story" open={!folded}>
+      <summary>{title}</summary>
+      {children}
+    </details>
+  );
+}
+
+export default Summary;
